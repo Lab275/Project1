f4f0122a580579784cd8edfa8022dded2123bed7
samleivadev <samdleiva@gmail.com>
2022-09-04 23:06:41 -0400

    adjuustment navbar

--- application/src/main/resources/static/js/react-client/src/components/header/index.js
+++ application/src/main/resources/static/js/react-client/src/components/header/index.js
@@ -5,21 +5,19 @@ import style from './headerStyle.module.scss';
 const Header = () => {
   return (
     <header className={style.header}>
-      <div className={style.section}>
-      <title className={style.title}>
+      
       <h1 className={style.title}>Stark Track</h1>
-      </title>
+     
       
     
       <nav className={style.navList}>
-      <ul className={style.navList}>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='/support'>CTDI</NavLink>
-          <NavLink to='/userpage'>Associate</NavLink>
-         
-      </ul>
+        <ul className={style.navList}>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/support'>CTDI</NavLink>
+            <NavLink to='/userpage'>Associate</NavLink>
+        </ul>
       </nav>
-   </div>
+  
       </header>
   );
 }
